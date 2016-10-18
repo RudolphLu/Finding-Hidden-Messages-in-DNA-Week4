a0ef34f82c5131f9e67f74ce04c567cfe779d801
Rudolph <CHLU@cypress.com>
2016-10-18 17:00:39 +0800

    Update homework

--- 1_3_4.c
+++ 1_3_4.c
@@ -1,5 +1,5 @@
 /*
- * RandomizedMotifSearch
+ * Gibbs Sampling
  * Delete the latest "\n" from input file
  */
 #include <stdio.h>
@@ -15,11 +15,12 @@ typedef unsigned int     UINT32;
 
 
 #define MAX_ARRAY_SIZE   1000000
-#define MAX_PROCESS_TIME 1000
+#define MAX_PROCESS_TIME 60
 
-#define SCAN_KMER        0
-#define SCAN_NUMOFDNA    1
-#define SCAN_DNA         2
+#define SCAN_KMER         0
+#define SCAN_NUMOFDNA     1
+#define SCAN_DNA          2
+#define SCAN_PROCESS_TIME 3
 
 
 typedef struct {
@@ -32,11 +33,10 @@ typedef struct {
 
 UINT32 alpha[256] = {0};
 
-
 /*
  * Read and parse data.
  */
-bool read_test_data(const char* file_name,DNA_INFO_t *dna_info,int maxsize) 
+bool read_test_data(const char* file_name,DNA_INFO_t *dna_info,UINT32 *N,int maxsize) 
 {    
     UINT32 phase = SCAN_KMER,count=0;
     UINT8 *src = dna_info->src;
@@ -46,7 +46,7 @@ bool read_test_data(const char* file_name,DNA_INFO_t *dna_info,int maxsize)
     if (!file) return false;
     if (!src)  return false;
     
-    dna_info->total_size = dna_info->num_of_dna = dna_info->kmer = 0;
+    *N = dna_info->total_size = dna_info->num_of_dna = dna_info->kmer = 0;
     
     do {
         if(SCAN_DNA == phase){
@@ -70,15 +70,26 @@ bool read_test_data(const char* file_name,DNA_INFO_t *dna_info,int maxsize)
         
         if(SCAN_NUMOFDNA == phase){
             c = fgetc(file);                     
-            if((c == 0xd) || (c == 0xa)){
-                if(0xa == c)
-                    phase = SCAN_DNA;
+            if(c == ' '){
+                phase = SCAN_PROCESS_TIME;
                 continue;
             }
 
             dna_info->num_of_dna *= 10;
             dna_info->num_of_dna += c-'0';
-        }        
+        }
+        
+        if(SCAN_PROCESS_TIME == phase){
+            c = fgetc(file);
+            if((c == 0xd) || (c == 0xa)){
+                if(0xa == c)
+                    phase = SCAN_DNA;
+                continue;
+            }
+            
+            *N *= 10;
+            *N += c-'0';
+        }
 
     } while (!feof (file) && count < maxsize);
     
@@ -147,23 +158,23 @@ UINT32 score_mmotifs(UINT8 *score_dna,DNA_INFO_t *dna_info)
  * Generate motifs profile.
  * motifs:
  * profile_pattern:
- * kmer:
- * num_of_dna:
+ * dna_info:
+ * ith: except ith sequence.
  */
-void gen_profile(UINT8 *motifs,UINT32 *profile_pattern,DNA_INFO_t *dna_info)
+void gen_profile(UINT8 *motifs,UINT32 *profile_pattern,DNA_INFO_t *dna_info,UINT32 ith_except)
 {
     UINT32 i,j;
     UINT32 a_cnt,c_cnt,g_cnt,t_cnt;
     
     //Generate profile
-    for(i=0;i<dna_info->kmer;i++){                
+    for(i=0;i<dna_info->kmer;i++){  
         for(a_cnt=c_cnt=g_cnt=t_cnt=j=0;j<dna_info->num_of_dna;j++){
+            if(ith_except==j) continue;
             if(motifs[j*dna_info->kmer+i] == 'A') a_cnt++;
             if(motifs[j*dna_info->kmer+i] == 'C') c_cnt++;
             if(motifs[j*dna_info->kmer+i] == 'G') g_cnt++;
             if(motifs[j*dna_info->kmer+i] == 'T') t_cnt++;
         }
-        
         profile_pattern[i]                  = a_cnt+1;
         profile_pattern[dna_info->kmer+i]   = c_cnt+1;
         profile_pattern[2*dna_info->kmer+i] = g_cnt+1;
@@ -175,13 +186,13 @@ void gen_profile(UINT8 *motifs,UINT32 *profile_pattern,DNA_INFO_t *dna_info)
 /*
  * Limit to 1000 times
  * dna_info: .
- * profile_pattern: .
  * best_motifs: The first randon select motifs.
+ * N: The number of the random process.
  */
-UINT32 RandomizedMotifSearch(DNA_INFO_t *dna_info,UINT32 *profile_pattern,UINT8 *best_motifs) 
+UINT32 GibbsSampler(DNA_INFO_t *dna_info,UINT32 *profile_pattern,UINT8 *best_motifs,UINT32 N) 
 {
     UINT32 score=0;
-    UINT32 i,j;
+    UINT32 i,j,ith_except;
     UINT8  *dna_src = dna_info->src;
     const UINT32 kmer = dna_info->kmer;
     const UINT32 num_dna = dna_info->num_of_dna;
@@ -189,53 +200,45 @@ UINT32 RandomizedMotifSearch(DNA_INFO_t *dna_info,UINT32 *profile_pattern,UINT8 
     UINT32 best_score;
     UINT8  *motifs = malloc(kmer*num_dna);    
     double m_prob,c_prob;
-    
-    
 
     //Random select motifs from DNA strings.
-    for(i=0;i<num_dna;i++){        
+    for(i=0;i<num_dna;i++){
         j = rand()%(size_dna-kmer);
         memcpy(best_motifs+i*kmer,dna_src+i*size_dna+j,kmer);
     }
     
-    // Generate profile of motifs
-    gen_profile(best_motifs,profile_pattern,dna_info);
+    memcpy(motifs,best_motifs,kmer*num_dna);
     
     // Get the score from first random motifs.
     best_score = score_mmotifs(best_motifs,dna_info);
-
-    while(1){
-        //Get the best motifs by profile.
-        for(i=0;i<num_dna;i++){
-            for(j=0,m_prob=0,memcpy(motifs+i*kmer,dna_src+i*size_dna,kmer);j<=(size_dna-kmer);j++){              
-                  c_prob = caculate_pr(dna_src+j+i*size_dna,profile_pattern,kmer);
+    
+    for(i=0;i<N;i++){
+        // i ¡ö Random(t)
+        ith_except = rand()%num_dna;
+        // Profile ¡ö profile matrix constructed from all strings in Motifs except for Motifi
+        gen_profile(motifs,profile_pattern,dna_info,ith_except);
+        //Motifi ¡ö Profile-randomly generated k-mer in the i-th sequence
+        for(j=0,m_prob=0;j<=(size_dna-kmer);j++){              
+                  c_prob = caculate_pr(dna_src+j+ith_except*size_dna,profile_pattern,kmer);
                   if(c_prob>m_prob){
                       m_prob = c_prob;
                       //Update the max probabity position
-                      memcpy(motifs+i*kmer,dna_src+j+i*size_dna,kmer);
+                      memcpy(motifs+ith_except*kmer,dna_src+j+ith_except*size_dna,kmer);
                   }
-            }
         }
-
         //Score
         score = score_mmotifs(motifs,dna_info);
         if(score<best_score){
-            //Create new profile.
-            gen_profile(motifs,profile_pattern,dna_info);
             best_score = score;
             memcpy(best_motifs,motifs,kmer*num_dna);
         }
-        else
-            break;        
     }
-    free(motifs);
-    
     return best_score;
 }
 
 void main()
 {
-    UINT32 i=0,j=0;
+    UINT32 i=0,j=0,N=0;
     UINT32 pr_multi,max_pr=0;
     UINT32 *profile_pattern;
     DNA_INFO_t dna_info;
@@ -244,10 +247,10 @@ void main()
     time_t t;
         
     dna_info.src = malloc(MAX_ARRAY_SIZE);
-    if(!read_test_data("dataset_158_9.txt",&dna_info,MAX_ARRAY_SIZE))
+    if(!read_test_data("dataset_158_9.txt",&dna_info,&N,MAX_ARRAY_SIZE))
         return;
 #ifdef DEBUG
-    printf("num_d:%d dna_total:%d dna_sz:%d kmer:%d \n",dna_info.num_of_dna,dna_info.total_size,dna_info.size_of_each_dna,dna_info.kmer);
+    printf("num_d:%d dna_total:%d dna_sz:%d kmer:%d process%d \n",dna_info.num_of_dna,dna_info.total_size,dna_info.size_of_each_dna,dna_info.kmer,N);
 #endif
     // Init hash index.
     alpha['A'] = 0;
@@ -263,15 +266,19 @@ void main()
     motifs = malloc(dna_info.kmer*dna_info.num_of_dna);
     //Initialize random info.
     srand((unsigned) time(&t));
+    
     for(i=0,score=0xffffffff;i<MAX_PROCESS_TIME;i++){
-        j = RandomizedMotifSearch(&dna_info,profile_pattern,motifs);
+        j = GibbsSampler(&dna_info,profile_pattern,motifs,N);
         if(j<score){
             score = j;
             memcpy(best_motifs,motifs,dna_info.kmer*dna_info.num_of_dna);
         }
     }
-        
-    
+
+#ifdef DEBUG
+    printf("score %d \n",score);
+#endif
+
     for(i=0;i<dna_info.num_of_dna;i++){
         for(j=0;j<dna_info.kmer;j++){
             printf("%c",best_motifs[i*dna_info.kmer+j]);
@@ -280,5 +287,4 @@ void main()
     }
     
     free(best_motifs);
-    free(motifs);
 }
